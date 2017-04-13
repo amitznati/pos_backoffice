2082bad41de20a2db63e1310afe45eeedf953ef3
amitznati <amitznati@gmail.com>
2017-04-13 13:03:15 -0700

    Users seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name'       => 'Amit Znati',
+            'email'  	 => 'amitznati@gmail.com',
+            'password'   => '$2y$10$bxsTsV/B89NXzZ8EaHvnYubq9/e7p/NLiCpLacRyG7SC806l6hmeu',
+        ]);
+    }
+}
